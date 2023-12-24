93d6cb657d976e55c061d4806594348019cd747f
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2023-12-24 17:25:53 +0100

    adding widgets to the addEntry screen

--- dailydiary/lib/addEntry.dart
+++ dailydiary/lib/addEntry.dart
@@ -1,12 +1,24 @@
 import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 class AddEntry extends StatelessWidget{
   final CameraDescription firstCamera;
+
   const AddEntry({super.key, required this.firstCamera});
 
   @override
   Widget build(BuildContext context) {
+    var entryController = TextEditingController();
+    var titleController = TextEditingController();
+    var moodController = TextEditingController();
+    var submitController = TextEditingController();
+    DateTime today = DateTime.now();
+    String dateStr = "${today.day}-${today.month}-${today.year}";
+    var title;
+
+    var score;
+    String datePrint = "Journal Date: " + dateStr;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add a new Entry', style: TextStyle(color: Colors.black)),
@@ -23,8 +35,123 @@ class AddEntry extends StatelessWidget{
           )
         ],
       ),
+      body: Container(
+        alignment: Alignment.topCenter,
+        margin: const EdgeInsets.symmetric(horizontal: 30),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: Text(
+                  datePrint,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.openSans(
+                      color: Colors.blue, fontSize: 24),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Give Your It A Title?',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child:TextFormField(
+                    controller: entryController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please complete the form';
+                    }
+                    return null;
+                  },
+                    decoration: const InputDecoration(
+                      labelText: 'Your Entry Goes Here',
+                      border: OutlineInputBorder(),
+                    ),
+                    maxLines: 8,
+                    minLines: 3,
+                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child:InkWell(
+                            onTap: () {
+                              score = 10;
+                              },
+                            child: Ink.image(
+                              image: const AssetImage('assets/thumbs_up.png'),
+                              // fit: BoxFit.cover,
+                              width: 70,
+                              height: 70,
+                            ),
+                          ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child:InkWell(
+                        onTap: () {
+                          score = 5;
+                          },
+                        child: Ink.image(
+                          image: const AssetImage('assets/neutral.png'),
+                          // fit: BoxFit.cover,
+                          width: 70,
+                          height: 70,
+                        ),
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child:InkWell(
+                          onTap: () {
+                            score = 0;
+                          },
+                          child: Ink.image(
+                            image: const AssetImage('assets/thumbs_down.png'),
+                            // fit: BoxFit.cover,
+                            width: 70,
+                            height: 70,
+                          ),
+                        ),
+                      )
+                    ]
+                  )
+                ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.blue,
+                    side: const BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text("Submit Entry?"),
+              ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
+
 }
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
